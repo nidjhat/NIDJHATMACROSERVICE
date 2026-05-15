125a78cf4f729e40686de40c39af2549dd7721f2
nidjhat👁‍🗨 <pnicat20@gmail.com>
2026-05-15 14:45:34 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,26 @@
+import customtkinter as ctk
 
-print("NIDJHATMACROSERVICE BASLADI")
+print("PROGRAM BASLADI")
+
+ctk.set_appearance_mode("dark")
+
+app = ctk.CTk()
+app.geometry("800x500")
+app.title("NIDJHATMACROSERVICE")
+
+label = ctk.CTkLabel(
+    app,
+    text="NIDJHATMACROSERVICE",
+    font=("Arial", 30, "bold")
+)
+label.pack(pady=40)
+
+button = ctk.CTkButton(
+    app,
+    text="PROGRAM CALISIYOR"
+)
+button.pack(pady=20)
+
+app.mainloop()
+
+input("ENTER BAS")
